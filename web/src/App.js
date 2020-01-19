8581192ae363d8aaeb39210e130f18da38cdc0ae
ldrf <ld.rf92@gmail.com>
2020-01-19 01:24:52 -0300

    Parte web Parte web

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,15 @@
-import React, { useState } from 'react';
+import './global.css'
+import './App.css'
+import './SiderBar.css'
+import './Main.css'
+
+import DevItem from './components/DevItem/index';
+import DevForm from './components/DevForm/index'
+
+
+
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 // Componente: Blocl isolado(Funçã)o que retorna algum conteudo HTML/CSS/JS , o qual não interfere no restante da aplicação
 // Propriedade: Informações  que um componente PAI para o componente FILHOs
@@ -8,21 +19,46 @@ import React, { useState } from 'react';
 import Header from './Header'
 
 function App() {
+  const [devs, setDevs] = useState([]);
 
-  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
 
-  
+      setDevs(response.data);
+    }
 
-  function incrementeCounter(){
-    setCounter(counter + 1);
+    loadDevs();
+  }, []);
+
+
+  async function handleAddDev(data) {
+    
+
+    const response = await api.post('/devs',data);
+
+    console.log(response.data);
+    
+
+    setDevs([...devs, response.data]);
   }
 
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick= {incrementeCounter}>Incrementar</button>
-    </>
-  );
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev}/>
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+            ))}
+        </ul>
+      </main>
+    </div>
+  )
 }
 
 export default App;
+
